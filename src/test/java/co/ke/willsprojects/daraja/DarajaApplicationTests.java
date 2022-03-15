6f509541c5199b9cdf72658f43247fbd0bd116f5
mwash-fred <mwangiwilly395@gmail.com>
2022-03-15 12:36:59 +0300

    Mpesa MicroService Initialization

---
+++ src/test/java/co/ke/willsprojects/daraja/DarajaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ke.willsprojects.daraja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarajaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
