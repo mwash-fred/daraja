ebd5b1c1fd8e2b9fca2969adc10ad1af76d8b5e2
mwash-fred <mwangiwilly395@gmail.com>
2022-07-12 18:04:30 +0300

    Core Url Configuration

--- src/main/java/co/ke/willsprojects/daraja/JsonSchemas/LoginRequest.java
+++ src/main/java/co/ke/willsprojects/daraja/JsonSchemas/LoginRequest.java
@@ -5,9 +5,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class LoginRequest {
     public String username;
     public String password;
+
+    public LoginRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
