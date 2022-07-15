c0f7b6a66d765cb9f4e4ec61c495c8bc29b5b990
mwash-fred <mwangiwilly395@gmail.com>
2022-07-15 13:48:47 +0300

    MPESA Transaction Posting

--- src/main/java/co/ke/willsprojects/daraja/Utils/CONSTANTS.java
+++ src/main/java/co/ke/willsprojects/daraja/Utils/CONSTANTS.java
@@ -1,11 +1,11 @@
 package co.ke.willsprojects.daraja.Utils;
 
 public class CONSTANTS {
-    public static final String authenticationUrl = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+    public static final String authenticationUrl = "https://api.safaricom.co.ke/oauth/v1/generate";
     public static final String c2bSimulationUrl = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate";
-    public static final String c2bUrlRegistrationUrl = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl";
-    public static final String B2C_TEST_INITIATOR_USERNAME = "testapi";
-    public static final String B2C_TEST_INITIATOR_PASSWD = "Safaricom977!";
+    public static final String c2bUrlRegistrationUrl = "https://api.safaricom.co.ke/mpesa/c2b/v1/registerurl";
+    public static final String B2C_TEST_INITIATOR_USERNAME = "GMUKOYA";
+    public static final String B2C_TEST_INITIATOR_PASSWD = "sudoFortune#1";
     public static final String b2cSimulationUrl = "https://sandbox.safaricom.co.ke/mpesa/b2c/v1/paymentrequest";
     public static final String connectUrl = "https://api.connect.fortunecredit.co.ke/";
     public static final String username = "DARAJA";
