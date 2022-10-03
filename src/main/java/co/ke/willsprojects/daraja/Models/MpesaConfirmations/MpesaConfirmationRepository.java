03166afa918fe18d0fe96d8269e886cf5f8873e5
mwash-fred <mwangiwilly395@gmail.com>
2022-10-03 10:39:07 +0300

    Scheduled Process

--- src/main/java/co/ke/willsprojects/daraja/Models/MpesaConfirmations/MpesaConfirmationRepository.java
+++ src/main/java/co/ke/willsprojects/daraja/Models/MpesaConfirmations/MpesaConfirmationRepository.java
@@ -3,6 +3,7 @@ package co.ke.willsprojects.daraja.Models.MpesaConfirmations;
 import co.ke.willsprojects.daraja.Interface.PartTran;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,5 +11,5 @@ public interface MpesaConfirmationRepository extends JpaRepository<MpesaConfirma
     MpesaConfirmation findByMpesaTransactionNo(String transactionNo);
 
     @Query(value = "select * from part_tran where tran_particulars regexp :code limit 1", nativeQuery = true)
-    PartTran mpesaPart(String mpesaCode);
+    PartTran mpesaPart(@Param("code") String mpesaCode);
 }
